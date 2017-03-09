4064128b48a8d312836455b6ea4420f92445d97b
dylmeadows <dylmeadows@gmail.com>
2017-03-08 21:58:07 -0500

    Incremental updates to codebase  - Add application plugin to build.gradle  - Removed UInt8 from JGameBoy driver to fix compileJava task breaking

--- src/main/java/JGameBoy.java
+++ src/main/java/JGameBoy.java
@@ -1,11 +1,7 @@
-import com.meadowsapps.jgameboy.core.util.Register8Bit;
-import com.meadowsapps.jgameboy.core.util.UInt8;
-
 /**
  * Created by dmeadows on 3/4/17.
  */
 public class JGameBoy {
     public static void main(String[] args) {
-        UInt8 uInt = new UInt8();
     }
 }
